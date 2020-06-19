4d2645e13f94e831813f28a7cce6024780af1b51
sugoi <promacanthus@qq.com>
2020-06-19 14:54:09 +0800

    Add array and test case

---
+++ array/array.go
@@ -0,0 +1,92 @@
+package array
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// 数组中数据为int类型
+
+type Array struct {
+	data   []int
+	length uint
+}
+
+func NewArray(capacity uint) *Array {
+	if capacity == 0 {
+		return nil
+	}
+	return &Array{
+		data:   make([]int, capacity, capacity),
+		length: 0,
+	}
+}
+
+// 获取数组长度
+func (a *Array) Len() uint {
+	return a.length
+}
+
+// 判断数组是否越界
+func (a *Array) isIndexOutOfRange(index uint) bool {
+	if index > uint(cap(a.data)) {
+		return true
+	}
+	return false
+}
+
+// 通过索引查找数组
+func (a *Array) Find(index uint) (int, error) {
+	if a.isIndexOutOfRange(index) {
+		return 0, errors.New("out of index range")
+	}
+
+	return a.data[index], nil
+}
+
+//  插入数据到索引index
+func (a Array) Insert(index uint, v int) error {
+	if a.length == uint(cap(a.data)) {
+		return errors.New("fully array")
+	}
+
+	if index != a.length && a.isIndexOutOfRange(index) {
+		return errors.New("out of index range")
+	}
+
+	for i := a.length; i > index; i-- {
+		a.data[i] = a.data[i-1]
+	}
+	a.data[index] = v
+	a.length++
+	return nil
+}
+
+// 插入在数组末尾
+func (a *Array) InsertToTail(v int) error {
+	return a.Insert(a.Len(), v)
+}
+
+//
+func (a *Array) Delete(index uint) (int, error) {
+	if a.isIndexOutOfRange(index) {
+		return 0, errors.New("out of index range")
+	}
+
+	v := a.data[index]
+	for i := index; i < a.length-1; i++ {
+		a.data[i] = a.data[i+1]
+	}
+	a.length--
+	return v, nil
+}
+
+// 打印数组
+func (a *Array) Print() {
+	var format string
+	for i := uint(0); i < a.length; i++ {
+		format += fmt.Sprintf("|%+v", a.data[i])
+	}
+	fmt.Println(format)
+}
---
+++ array/array_test.go
@@ -0,0 +1,268 @@
+package array
+
+import (
+	"testing"
+)
+
+func TestArray_Delete(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	type args struct {
+		index uint
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			"test_delete",
+			fields{
+				data:   []int{1, 2, 3},
+				length: 3,
+			},
+			args{index: uint(2)},
+			3,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			got, err := a.Delete(tt.args.index)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Delete() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestArray_Find(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	type args struct {
+		index uint
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			"test_find",
+			fields{
+				data:   []int{1, 2, 3},
+				length: 3,
+			},
+			args{index: uint(1)},
+			2,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			got, err := a.Find(tt.args.index)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Find() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Find() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestArray_Insert(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	type args struct {
+		index uint
+		v     int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"test_insert",
+			fields{
+				data:   []int{1, 2, 3},
+				length: 3,
+			},
+			args{
+				index: uint(3),
+				v:     4,
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			if err := a.Insert(tt.args.index, tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Insert() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestArray_InsertToTail(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	type args struct {
+		v int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"test_inserttotail",
+			fields{
+				data:   make([]int, 3, 4),
+				length: 0,
+			},
+			args{v: 4},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			if err := a.InsertToTail(tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("InsertToTail() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestArray_Len(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   uint
+	}{
+		{
+			"test_len",
+			fields{
+				data:   []int{1, 2, 3},
+				length: 3,
+			},
+			uint(3),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			if got := a.Len(); got != tt.want {
+				t.Errorf("Len() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestArray_Print(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			"test_print",
+			fields{
+				data:   []int{1, 2, 3},
+				length: 3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			a.Print()
+		})
+	}
+}
+
+func TestArray_isIndexOutOfRange(t *testing.T) {
+	type fields struct {
+		data   []int
+		length uint
+	}
+	type args struct {
+		index uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			"test_isindexoutofrange",
+			fields{
+				data:   []int{1, 2, 3},
+				length: 3,
+			},
+			args{index: uint(0)},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Array{
+				data:   tt.fields.data,
+				length: tt.fields.length,
+			}
+			if got := a.isIndexOutOfRange(tt.args.index); got != tt.want {
+				t.Errorf("isIndexOutOfRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
