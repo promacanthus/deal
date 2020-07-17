7a05b973b6722c37c1ac6b0a4801cf3f1d878bbd
sugoi <promacanthus@qq.com>
2020-07-17 15:17:21 +0800

    Use builtin sort function

--- example/no-15.go
+++ example/no-15.go
@@ -1,62 +1,40 @@
 package example
 
+// leetcode 第15题：三数之和
+
+import "sort"
+
 func threeSum(nums []int) [][]int {
-	res := make([][]int, 0)
-	if len(nums) < 3 {
-		return res
-	}
-
-	sortB(nums)
-	sortI(nums)
-	sortS(nums)
-
-	return res
-}
-
-// 冒泡
-func sortB(nums []int) {
 	n := len(nums)
-	sortBorder := n - 1
-	for i := 0; i < n-1; i++ {
-		flag := false
-		for j := 0; j < sortBorder; j++ {
-			if nums[j] > nums[j+1] {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
-				flag = true
-			}
-		}
-		if !flag {
-			break
-		}
-	}
-}
+	sort.Ints(nums)
+	ans := make([][]int, 0)
 
-// 插入
-func sortI(nums []int) {
-	for i := 1; i < len(nums); i++ {
-		value := nums[i]
-		j := i - 1
-		for ; j >= 0; j-- {
-			if value < nums[j] {
-				nums[j+1] = nums[j]
-			} else {
+	// 枚举 a
+	for i := 0; i < n; i++ {
+		// 需要和上一次枚举的数不相同
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		target := -1 * nums[i]
+		// 枚举 b
+		for j, k := i+1, n-1; j < n; j++ {
+			// 需要和上一次枚举的数不相同
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for j < k && nums[j]+nums[k] > target {
+				k--
+			}
+			// 如果指针重合，随着 b 后续的增加，就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+			if j == k {
 				break
 			}
-		}
-		nums[j+1] = value
-	}
-}
-
-// 选择
-func sortS(nums []int) {
-	n := len(nums)
-	for i := 0; i < n-1; i++ {
-		min := i
-		for j := i + 1; j < n; j++ {
-			if nums[min] > nums[j] {
-				min = j
+			if nums[j]+nums[k] == target {
+				ans = append(ans, []int{nums[i], nums[j], nums[k]})
 			}
 		}
-		nums[i], nums[min] = nums[min], nums[i]
 	}
+	return ans
 }
