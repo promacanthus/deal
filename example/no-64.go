7d8fa8d6f7cb77c12220de2856f1ae16dbc37f37
sugoi <promacanthus@qq.com>
2020-07-21 19:44:54 +0800

    Add dynamic programming solution

--- example/no-64.go
+++ example/no-64.go
@@ -1,24 +1,60 @@
 package example
 
-// 最小路径和
-
 import "math"
 
-// 分治
+// 最小路径和
+
+// 回溯+缓存
 func minPathSum1(grid [][]int) int {
-	return int(cal(grid, 0, 0))
+	return min(grid, 0, 0)
 }
 
-func cal(grid [][]int, i, j int) float64 {
-	// 走到最边上了
+func min(grid [][]int, i, j int) int {
 	if i == len(grid) || j == len(grid[0]) {
 		return math.MaxInt64
 	}
 
-	// 走到右下角了
 	if i == len(grid)-1 && j == len(grid[0])-1 {
-		return float64(grid[i][j])
+		return grid[i][j]
+	}
+	return grid[i][j] + minimum(min(grid, i, j+1), min(grid, i+1, j))
+}
+
+func minimum(a, b int) int {
+	if a >= b {
+		return b
+	}
+	return a
+}
+
+// 动态规划
+func minPathSum2(grid [][]int) int {
+	iLength := len(grid)
+	if iLength < 1 {
+		return 0
+	}
+	jLength := len(grid[0])
+
+	// 初始化状态转移表
+	dp := make([][]int, iLength)
+	for i, state := range grid {
+		dp[i] = make([]int, len(state))
 	}
 
-	return float64(grid[i][j]) + math.Min(cal(grid, i+1, j), cal(grid, i, j+1))
+	// 第一行特殊处理
+	dp[0][0] = grid[0][0]
+
+	// 	动态规划
+	for i := 0; i < iLength; i++ {
+		for j := 0; j < jLength; j++ {
+			if i == 0 && j != 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if i != 0 && j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			} else if i != 0 && j != 0 {
+				dp[i][j] = minimum(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return dp[iLength-1][jLength-1]
 }
