41127eb8c72570eb72e6e586717c7cf7ae7491b4
sugoi <promacanthus@qq.com>
2020-07-22 17:26:49 +0800

    Update

--- array/array.go
+++ array/array.go
@@ -36,7 +36,6 @@ func (a *Array) Find(index uint) (int, error) {
 	if a.isIndexOutOfRange(index) {
 		return 0, errors.New("out of index range")
 	}
-
 	return a.data[index], nil
 }
 
@@ -50,9 +49,15 @@ func (a Array) Insert(index uint, v int) error {
 		return errors.New("out of index range")
 	}
 
+	// 移动数组，空出位置
 	for i := a.length; i > index; i-- {
 		a.data[i] = a.data[i-1]
 	}
+
+	// 将待插入位置的数据放到数组末尾，空出位置
+	// 保证数组有序，则不能用这个方法
+	// a.data[a.length] = a.data[index]
+
 	a.data[index] = v
 	a.length++
 	return nil
@@ -60,25 +65,29 @@ func (a Array) Insert(index uint, v int) error {
 
 // 插入在数组末尾
 func (a *Array) InsertToTail(v int) error {
-	return a.Insert(a.Len(), v)
+	return a.Insert(a.length, v)
 }
 
 //
 func (a *Array) Delete(index uint) (int, error) {
 	if a.isIndexOutOfRange(index) {
-		return 0, errors.New("out of index range")
+		return -1, errors.New("out of index range")
 	}
 
 	v := a.data[index]
+	// 移动数组，填上空出位置
 	for i := index; i < a.length-1; i++ {
 		a.data[i] = a.data[i+1]
 	}
+	// 将数组末尾的数据放到待插入位置
+	// 保证数组有序，则不能用这个方法
+	// a.data[index] = a.data[a.length-1]
 	a.length--
 	return v, nil
 }
 
 // 打印数组
-func (a *Array) Print() {
+func (a *Array) String() {
 	var format string
 	for i := uint(0); i < a.length; i++ {
 		format += fmt.Sprintf("|%+v", a.data[i])
