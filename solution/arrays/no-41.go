ee0137d5a7d8e9cdca16a753536b91ee69578bc5
sugoi <promacanthus@qq.com>
2020-07-27 10:30:14 +0800

    Add solution to 41

---
+++ solution/arrays/no-41.go
@@ -0,0 +1,51 @@
+package arrays
+
+import (
+	"sort"
+)
+
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	// empty
+	if n < 1 {
+		return 1
+	}
+
+	index := -1
+	sort.Ints(nums)
+
+	// remove duplicates
+	res := make([]int, 0)
+	for i := 0; i < n; i++ {
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
+		}
+		res = append(res, nums[i])
+	}
+
+	for i, num := range res {
+		if num > 0 {
+			index = i
+			break
+		}
+	}
+	n = len(res)
+	// all negative or index value is not 1
+	if index < 0 || res[index] != 1 {
+		return 1
+	}
+
+	return dmp(res, n, index, 2)
+}
+
+func dmp(res []int, n, index, point int) int {
+	// traverse to the end
+	if index == n-1 {
+		return res[index] + 1
+	}
+	// somewhere in the middle
+	if res[index+1] > point {
+		return point
+	}
+	return dmp(res, n, index+1, point+1)
+}
---
+++ solution/arrays/no-41_test.go
@@ -0,0 +1,73 @@
+package arrays
+
+import "testing"
+
+func Test_firstMissingPositive(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			"0",
+			args{[]int{4, 3, 4, 1, 1, 4, 1, 4}},
+			2,
+		},
+		{
+			"1",
+			args{[]int{1, 2, 0}},
+			3,
+		},
+		{
+			"2",
+			args{[]int{3, 4, -1, 1}},
+			2,
+		},
+		{
+			"3",
+			args{[]int{7, 8, 9, 11, 12}},
+			1,
+		},
+		{
+			"4",
+			args{[]int{}},
+			1,
+		},
+		{
+			"5",
+			args{[]int{2}},
+			1,
+		},
+		{
+			"6",
+			args{[]int{1}},
+			2,
+		},
+		{
+			"7",
+			args{[]int{0}},
+			1,
+		},
+		{
+			"8",
+			args{[]int{0, 1}},
+			2,
+		},
+		{
+			"9",
+			args{[]int{1, 1}},
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstMissingPositive(tt.args.nums); got != tt.want {
+				t.Errorf("firstMissingPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
