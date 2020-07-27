3cadb5a040f379369964163ca0b23e11a9214a30
sugoi <promacanthus@qq.com>
2020-07-27 10:29:38 +0800

    Rename testing function

--- solution/arrays/no-15_test.go
+++ solution/arrays/no-15_test.go
@@ -26,7 +26,7 @@ func Test_threeSum(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := threeSumDP(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("threeSumDP() = %v, want %v", got, tt.want)
 			}
 		})
