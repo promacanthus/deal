1f80f4218fb437037b67daedab841a501a99eae7
Bowen Zhu <Promacanthus@gmail.com>
2020-07-28 21:22:32 +0800

    Add Greedy Algorithm soultion

--- solution/stack/no-32.go
+++ solution/stack/no-32.go
@@ -49,3 +49,33 @@ func longestValidParenthesesDP(s string) int {
 	}
 	return maxAns
 }
+
+func longestValidParenthesesGreed(s string) int {
+	left, right, maxLength := 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			maxLength = max(maxLength, 2*right)
+		} else if right > left {
+			left, right = 0, 0
+		}
+	}
+	left, right = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			maxLength = max(maxLength, 2*left)
+		} else if left > right {
+			left, right = 0, 0
+		}
+	}
+	return maxLength
+}
