b4eb27e74bd9c6e0e5bc4320dba3599535403146
sugoi <promacanthus@qq.com>
2020-08-03 16:17:20 +0800

    👌 IMPROVE: add comment

--- sort/selectionsort.go
+++ sort/selectionsort.go
@@ -1,5 +1,10 @@
 package sort
 
+// 数组实现的选择排序是不稳定的
+
+// 如待排序[]int{5,5,2}，选择排序的结果是[]int{2,5,5}
+// 其中两个5的前后顺序变了
+
 func SelectionSort(list []int) {
 	n := len(list)
 	if n <= 1 {
@@ -7,7 +12,9 @@ func SelectionSort(list []int) {
 	}
 
 	for i := 0; i < n-1; i++ {
+		// 外层循环控制待交换的位置
 		min := i
+		// 从第二个元素开始，查找最小的元素，与i交换
 		for j := i + 1; j < n; j++ {
 			// 查找最小的元素
 			if list[min] > list[j] {
