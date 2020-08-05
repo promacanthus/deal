8e1f47066001e058ca128df9e58752d03b333dc4
sugoi <promacanthus@qq.com>
2020-08-05 13:18:35 +0800

    👌 IMPROVE: optimize the heap

--- heap/heap.go
+++ heap/heap.go
@@ -2,71 +2,89 @@ package heap
 
 type Heap struct {
 	items []int
-	count uint
+	los   bool // true == large heap;false == small heap
 }
 
-func NewHeap() *Heap {
-	items := make([]int, 1)
-	return &Heap{items: items, count: 0}
-}
-
-// max heap
-func (h *Heap) Insert(v int) {
-	h.items = append(h.items, v)
-	h.count++
-	update(h, v)
-}
-
-func update(heap *Heap, v int) {
-	index := heap.count
-	par := index / 2
-	for v > heap.items[par] && par > 0 {
-		heap.items[par], heap.items[index] = heap.items[index], heap.items[par]
-		par, index = par/2, par
+func NewHeap(flag bool) *Heap {
+	return &Heap{
+		items: make([]int, 0),
+		los:   flag,
 	}
 }
 
-func (h *Heap) Get() (int, bool) {
-	if h.count == 0 {
-		return -1, false
+func InitHeap(values []int, flag bool) *Heap {
+	res := NewHeap(flag)
+	for i := 0; i < len(values); i++ {
+		res.Push(values[i])
 	}
-	return h.items[1], true
-}
-
-func (h *Heap) Delete() (int, bool) {
-	if h.count == 0 {
-		return -1, false
-	}
-	h.items[1], h.items[h.count] = h.items[h.count], h.items[1]
-	h.count--
-	res := heapify(h, h.count)
-	return res, true
-}
-
-func heapify(h *Heap, count uint) int {
-	for i := uint(1); i < count/2; {
-		maxIndex := i
-		if h.items[i] < h.items[i*2] {
-			maxIndex = i * 2
-		}
-
-		if i*2+1 <= count && h.items[maxIndex] < h.items[i*2+1] {
-			maxIndex = i*2 + 1
-		}
-
-		if maxIndex == i {
-			break
-		}
-
-		h.items[i], h.items[maxIndex] = h.items[maxIndex], h.items[i]
-	}
-	res := h.items[count]
 	return res
 }
 
-func (h *Heap) Sort() {
+func (h *Heap) len() int {
+	return len(h.items)
+}
 
-	for i := uint(1); i <= h.count; i++ {
-		h.Delete()
+func (h *Heap) less(i, j int) bool {
+	if h.los {
+		return h.items[i] > h.items[j]
 	}
+	return h.items[i] < h.items[j]
+}
+
+func (h *Heap) swap(i, j int) {
+	h.items[i], h.items[j] = h.items[j], h.items[i]
+}
+
+func (h *Heap) Push(v int) {
+	h.items = append(h.items, v)
+	up(h, h.len()-1)
+}
+
+func up(h *Heap, i int) {
+	for {
+		p := (i - 1) / 2 // parent
+		if p == i || !h.less(i, p) {
+			break
+		}
+		h.swap(p, i)
+		i = p
+	}
+}
+
+func (h *Heap) Pop() int {
+	n := h.len() - 1
+	h.swap(0, n)
+	down(h, 0, n)
+	res := h.items[n]
+	h.items = h.items[:n]
+	return res
+}
+
+func down(h *Heap, root int, n int) {
+	i := root
+	for {
+		l := 2*i + 1 // left
+		if l >= n || l < 0 {
+			break
+		}
+		max := l
+		r := l + 1 // right
+		if r < n && h.less(r, l) {
+			max = r
+		}
+		if !h.less(max, i) {
+			break
+		}
+		h.swap(i, max)
+		i = max
+	}
+}
+
+func (h Heap) Sort() []int {
+	n := h.len()
+	res := make([]int, 0)
+	for i := 0; i < n; i++ {
+		res = append(res, h.Pop())
+	}
+	return res
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -1,151 +1,41 @@
 package heap
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
 var origin = []int{0, 33, 27, 21, 16, 13, 15, 19, 5, 6, 7, 8, 1, 2, 12}
 
-func TestHeap_Delete(t *testing.T) {
-	type fields struct {
-		items []int
-		count uint
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   int
-		want1  bool
-	}{
-		// TODO: Add test cases.
-		{
-			"1",
-			fields{
-				items: origin,
-				count: 13,
-			},
-			33,
-			true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			h := &Heap{
-				items: tt.fields.items,
-				count: tt.fields.count,
-			}
-			got, got1 := h.Delete()
-			if got != tt.want {
-				t.Errorf("Delete() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("Delete() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
-func TestHeap_Get(t *testing.T) {
-	type fields struct {
-		items []int
-		count uint
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   int
-		want1  bool
-	}{
-		// TODO: Add test cases.
-		{
-			"1",
-			fields{
-				items: origin,
-				count: 13,
-			},
-			33,
-			true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			h := &Heap{
-				items: tt.fields.items,
-				count: tt.fields.count,
-			}
-			got, got1 := h.Get()
-			if got != tt.want {
-				t.Errorf("Get() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("Get() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
-func TestHeap_Insert(t *testing.T) {
-	type fields struct {
-		items []int
-		count uint
-	}
-	type args struct {
-		v int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-		{
-			"1",
-			fields{
-				items: origin,
-				count: 13,
-			},
-			args{v: 22},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			h := &Heap{
-				items: tt.fields.items,
-				count: tt.fields.count,
-			}
-			h.Insert(tt.args.v)
-			fmt.Println(h.items)
-		})
-	}
-}
-
 func TestHeap_Sort(t *testing.T) {
 	type fields struct {
 		items []int
-		count uint
+		los   bool
 	}
 	tests := []struct {
 		name   string
 		fields fields
+		want   []int
 	}{
 		// TODO: Add test cases.
 		{
 			"1",
 			fields{
-				items: origin,
-				count: 14,
+				items: InitHeap(origin, false).items,
+				los:   false,
 			},
+			[]int{0, 1, 2, 5, 6, 7, 8, 12, 13, 15, 16, 19, 21, 27, 33},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := &Heap{
+			h := Heap{
 				items: tt.fields.items,
-				count: tt.fields.count,
+				los:   tt.fields.los,
 			}
-			h.Sort()
-			fmt.Println(h.items)
+			if got := h.Sort(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sort() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
