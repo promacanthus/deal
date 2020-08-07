614956d12c7b1c8ddc93c3bc58fdc98a757eb5f3
sugoi <promacanthus@qq.com>
2020-08-07 14:33:50 +0800

    👌 IMPROVE: add test case

---
+++ solution/DP/no-10_test.go
@@ -0,0 +1,24 @@
+package DP
+
+import "testing"
+
+func Test_isMatch(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
