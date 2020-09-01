d53ff42641fb0296fcb232e61f3e61df8d95d387
Bowen Zhu <Promacanthus@gmail.com>
2020-09-02 00:00:19 +0800

    📦 NEW: add bad solution

---
+++ leetcode/linkedlists/147_test.go
@@ -0,0 +1,55 @@
+package linkedlists
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_insertionSortList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{
+			"1",
+			args{&ListNode{
+				Val: 4,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 1,
+						Next: &ListNode{
+							Val:  3,
+							Next: nil,
+						},
+					},
+				},
+			}},
+			&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := insertionSortList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("insertionSortList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
